713bbbe1fcb72d317f2edd6ef05bda19d6769c8c
Fernando Barbalho <fbarbalho@hotmail.com>
2024-10-11 15:22:33 -0300

    scripts para fazer análise de PCA

--- laboratorio_pca.R
+++ laboratorio_pca.R
@@ -5,9 +5,71 @@ fab<-
   distinct(partner, partner_label)
 
 brazil_full %>%
-  filter(str_length(product) == 4,
+  filter(str_length(product) == 3,
          between(partner,1,1399)) %>%
   summarise(total = sum(us_dollars_at_current_prices_in_thousands),
             .by = c(partner_label, product_label)) %>%
   pivot_wider(names_from = product_label, values_from = total) %>%
-  slice_sample()
+  slice_sample(n=50) %>%
+  readr::write_csv("amostra_brazil_full.csv")
+
+dados_pca<-
+  brazil_full %>%
+  filter(str_length(product) == 3,
+         between(partner,1,1399)) %>%
+  summarise(total = sum(us_dollars_at_current_prices_in_thousands),
+            .by = c(partner_label, product_label)) %>%
+  pivot_wider(names_from = product_label, values_from = total)
+
+dados_pca<- janitor::clean_names(dados_pca)
+
+dados_pca<-
+dados_pca %>%
+  replace(is.na(.), 0)
+
+# Carregar bibliotecas necessárias
+library(tidyverse)
+library(factoextra)
+
+# Carregar os dados
+data <- dados_pca
+
+# Verificar a estrutura dos dados
+str(data)
+
+# Remover a coluna de países e selecionar apenas colunas numéricas (produtos)
+data_numeric <- data %>%
+  select(-1) %>%
+  mutate(across(everything(), as.numeric))
+
+# Padronizar os dados (necessário para PCA)
+data_scaled <- scale(data_numeric)
+
+# Executar a PCA
+pca_result <- prcomp(data_scaled, center = TRUE, scale. = TRUE)
+
+# Resumir os resultados
+summary(pca_result)
+
+# Visualizar variância explicada por cada componente
+fviz_eig(pca_result)
+
+# Visualizar os países no espaço dos componentes principais
+fviz_pca_ind(pca_result, 
+             geom.ind = "point",
+             col.ind = "blue", 
+             addEllipses = TRUE,
+             ellipse.level = 0.95)
+
+# Visualizar a contribuição de cada produto para os componentes principais
+fviz_pca_var(pca_result, 
+             col.var = "contrib", 
+             gradient.cols = c("blue", "yellow", "red"),
+             repel = TRUE)
+
+# Visualizar biplot (países e produtos juntos)
+fviz_pca_biplot(pca_result, 
+                geom.ind = "point", 
+                col.ind = "blue", 
+                col.var = "red",
+                repel = TRUE)
